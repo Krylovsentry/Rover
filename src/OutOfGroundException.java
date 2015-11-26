a457280ce0baa0d932c6b62063edb0f149aa6523
Anton Krylov <krylovsentry@yandex.ru>
2015-11-26 20:59:49 +0300

    New Class Exception

---
+++ src/OutOfGroundException.java
@@ -0,0 +1,16 @@
+
+/**
+ * Created by Антон on 26.11.2015.
+ */
+public class OutOfGroundException extends Exception {
+
+
+    public OutOfGroundException(String message){
+
+
+        super(message);
+
+
+    }
+
+}
